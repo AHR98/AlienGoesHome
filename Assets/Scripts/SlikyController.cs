af11a16a8996b538a3b413df3f79d73765136e12
Alejandra Huerta Rangel <rangeel82@gmail.com>
2022-08-16 22:22:15 -0500

    Slinky move forward, backward, left, right, dance, and turn around
    Slinky move forward, backward, left, right, dance, and turn around

--- Assets/Scripts/SlikyController.cs
+++ Assets/Scripts/SlikyController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SlikyController : MonoBehaviour
 {
     private Animator animController;
+  
     // Start is called before the first frame update
     void Start()
     {
@@ -18,8 +19,26 @@ public class SlikyController : MonoBehaviour
         animController.SetFloat("Speed", Input.GetAxis("Vertical"));
         animController.SetFloat("Direction", Input.GetAxis("Horizontal"));
 
+        if(Input.GetAxis("Vertical") > 0.1f 
+            || Input.GetAxis("Horizontal") > 0.0f 
+            || Input.GetAxis("Horizontal") < 0.0f) //Forward 
+        {
+            animController.SetBool("Run", true);
+        }
+        else if (Input.GetAxis("Vertical") < 0.0f) //Back
+        {
+            animController.SetBool("TurnAround", true);
+          //  animController.SetBool("Run", true);
+        }
+ 
+        else
+        {
+            animController.SetBool("TurnAround", false);
+            animController.SetBool("Run", false);
+        }
+
         //Special Dance
-        if(Input.GetKey(KeyCode.LeftShift) )
+        if (Input.GetKey(KeyCode.LeftShift) )
             if(Input.GetKeyUp(KeyCode.D))
                 animController.SetTrigger("Dance");
         
