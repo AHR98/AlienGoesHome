2491600d213ffa6969ea655d51587913d1bb63da
Alejandra Huerta Rangel <rangeel82@gmail.com>
2022-08-20 23:59:13 -0500

    Slinky has a weapon!
    Blend tree 2d simple direction, slinky has a gun, slinky can walk backward

--- Assets/Scripts/SlikyController.cs
+++ Assets/Scripts/SlikyController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class SlikyController : MonoBehaviour
 {
     private Animator animController;
-  
+    private float horizontalDirection;
+    private float verticalDirection;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,44 +16,56 @@ public class SlikyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalDirection = Input.GetAxis("Horizontal");
+        verticalDirection = Input.GetAxis("Vertical");
         //Special Dance
         if (Input.GetKey(KeyCode.LeftShift))
             if (Input.GetKeyUp(KeyCode.Z))
                 animController.SetTrigger("Dance");
 
-        //Walk-run
-        animController.SetFloat("Speed", Input.GetAxis("Vertical"));
-        animController.SetFloat("Direction", Input.GetAxis("Horizontal"));
 
-        if(Input.GetAxis("Vertical") > 0.1f 
-            || (Input.GetAxis("Vertical") > 0.5f && Input.GetAxis("Horizontal") > 0.5f)
-            || (Input.GetAxis("Vertical") > 0.5f && Input.GetAxis("Horizontal") < -0.5f) ) 
+
+        //Walk-run
+        animController.SetFloat("Speed", verticalDirection);
+        animController.SetFloat("Direction", horizontalDirection);
+        if (verticalDirection != 0.0f || horizontalDirection != 0.0f)
         {
             animController.SetBool("Run", true);
         }
-        else if (Input.GetAxis("Vertical") < 0.0f) //Back
-        {
-            animController.SetBool("TurnAroundRight", true);
-          //  animController.SetBool("Run", true);
-        }
-        else if(Input.GetAxis("Horizontal") > 0.0f && Input.GetAxis("Vertical") == 0.0f)
-        {
-            animController.SetBool("TurnAroundRight", true);
-        }
-        else if(Input.GetAxis("Horizontal") < 0.0f && Input.GetAxis("Vertical") == 0.0f)
-        {
-            animController.SetBool("TurnAroundLeft", true);
-
-        }
         else
         {
-            animController.SetBool("TurnAroundRight", false);
-            animController.SetBool("TurnAroundLeft", false);
             animController.SetBool("Run", false);
         }
 
-        
-        
+        //if (verticalDirection > 0.1f
+        //    || (verticalDirection > 0.5f && horizontalDirection > 0.5f
+        //    || verticalDirection > 0.5f && horizontalDirection < -0.5f))
+        //{
+        //    animController.SetBool("Run", true);
+        //}
+        //else if (verticalDirection < 0.0f) //Back
+        //{
+        //    animController.SetBool("TurnAroundRight", true);
+        //    //  animController.SetBool("Run", true);
+        //}
+        //else if (horizontalDirection > 0.0f && verticalDirection == 0.0f)
+        //{
+        //    animController.SetBool("TurnAroundRight", true);
+        //}
+        //else if (horizontalDirection < 0.0f && verticalDirection == 0.0f)
+        //{
+        //    animController.SetBool("TurnAroundLeft", true);
+
+        //}
+        //else
+        //{
+        //    animController.SetBool("TurnAroundRight", false);
+        //    animController.SetBool("TurnAroundLeft", false);
+        //    animController.SetBool("Run", false);
+        //}
+
+
+
     }
 
     private void OnCollisionEnter(Collision collision)
